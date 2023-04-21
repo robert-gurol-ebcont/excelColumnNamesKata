34418283d552af17dff26f8e2df5269c9feaef3d
Robert Gurol <robert.gurol@ebcont.com>
2023-04-21 20:45:21 +0200

    work in progress

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,30 @@ package org.example;
 public class Main {
 
     public static String toCellReference(int columnNumber, int rowNumber) {
-        return "";
+        String rowPart = Integer.toString(rowNumber + 1);
+        StringBuilder columnBuilder = new StringBuilder();
+        if (columnNumber == 0) {
+            return "A" + rowPart;
+        }
+        int currentNumber = columnNumber;
+        boolean firstRound = true;
+        while (currentNumber > 0) {
+            int rest = currentNumber % 26;
+            if (firstRound) {
+                columnBuilder.insert(0, toLetterColumnDigit(rest));
+            } else {
+                columnBuilder.insert(0, toLetterColumnDigit(rest - 1));
+            }
+            currentNumber = (currentNumber - rest) / 26;
+            firstRound = false;
+        }
+        return columnBuilder + rowPart;
+    }
+
+    /**
+     * @param columnNumber number between
+     */
+    private static char[] toLetterColumnDigit(int columnNumber) {
+        return Character.toChars(65 + columnNumber);
     }
 }
